f28bbd4ee2498c6f346e18c79f04d94fd61c04c2
“Davit <2840850M@student.gla.ac.uk>
2026-05-17 22:27:02 +0100

    Initial commit

---
+++ ReframeAI/ReframeAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReframeAIApp.swift
+//  ReframeAI
+//
+//  Created by Davit Muradyan on 17.05.26.
+//
+
+import SwiftUI
+
+@main
+struct ReframeAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
